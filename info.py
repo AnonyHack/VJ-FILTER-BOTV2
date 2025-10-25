9be4b6d32f93b4e438cedbb26af8804e036afda2
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-10-25 11:31:55 +0200

    Update info.py

--- info.py
+++ info.py
@@ -89,9 +89,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Moviewasgrp')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Movies_Hub_Flix1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/MoviesHub_101')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MoviesHubFlixSupport') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Silando')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Am_itachiuchiha')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
